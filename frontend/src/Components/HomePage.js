d7c1955c43a6a4d24229adb13df7910353fcfc9d
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2022-08-23 18:34:10 +0800

    edit homepage title

--- frontend/src/Components/HomePage.js
+++ frontend/src/Components/HomePage.js
@@ -126,7 +126,7 @@ class HomePage extends React.Component {
         return (
             <div>
             <h1>
-                Hello Piggy!
+                Hello Cub!
             </h1>
             <ReactEcharts
                 option={this.getTwoCompareOption(this.state.TAIEX.x, this.state.TAIEX.y, this.state.fund.y)}
