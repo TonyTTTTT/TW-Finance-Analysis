b2c47575bbf9357e3d09c0c7081bee4f0660b5fe
TonyTTTTT <r09922176@csie.ntu.edu.tw>
2022-09-04 16:25:46 +0800

    update py script to deduplicate data in TAIEX.csv

--- anlyze-by-foreign-capital/del_duplicate_TAIEX.py
+++ anlyze-by-foreign-capital/del_duplicate_TAIEX.py
@@ -4,7 +4,7 @@ df_new = []
 with open('./data/TAIEX.csv', 'r') as f:
     df = pd.read_csv(f)
     for i in range(0, df.shape[0]):
-        if df.iloc[i]['Date'] == df.iloc[i-1]['Date']:
+        if df.iloc[i]['Date'] != df.iloc[i-1]['Date']:
             df_new.append(df.iloc[i])
 
 df_new = pd.DataFrame(df_new)
